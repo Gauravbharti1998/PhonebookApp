25e3eacf353670d7fef13f2ae6eda9ba9f214502
Gaurav Bharti <gaurav.bharti1998@gmail.com>
2020-05-31 17:33:30 +0530

    Added to file

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ let app = express()
 app.set('view engine', 'ejs')
 app.set('views', './views/ejs')
 
-app.get('/', (req, res) => {
-    res.render('prac', { array: ['Rishabh', 'Gaurav', 'Hello', 'World'] })
-})
+// This helps to process post requests
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
+app.get('/',(req,res)=>{
+    res.render('home')  // to convert ejs file to html to show on page of browser
+})
+app.post('/show',(req,res)=>{
+    console.log(req.body)
+    res.render('home2', {query: req.body})
+})
 app.listen("3000", console.log("Running on port 3000"))
